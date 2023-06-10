21948b8edc82526d0849b2648df64a09ae26cbeb
Andrey Soldatenkov <soldatenkov067@bk.ru>
2023-06-10 12:00:32 +0300

    add hw_2 by exception

---
+++ Task4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Вы ввели строку: " + getString());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String getString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String userString = scanner.nextLine();
+        if (userString == "") {
+            throw new RuntimeException("Строка пуста! Так не должно быть");
+        }
+        return userString;
+    }
+}
